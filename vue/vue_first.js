2a9085dcebc77a60b3519c949b672f9462cd7a73
Tie Tung Huat <tunghuat.2000@gmail.com>
2022-07-20 21:30:21 +0800

    vue testing

---
+++ vue/vue_first.js
@@ -0,0 +1,12 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            message: 'Hello Vue!',
+            message2: 'It’s me!',
+            message3: 'King James！'
+            
+        }
+    }
+}).mount('#first')
