45bfd07d47bcca1cf697b3f26aa3625d486ad93c
Tie Tung Huat <tunghuat.2000@gmail.com>
2022-07-24 23:30:46 +0800

    7/24

--- vue/vue_review.js
+++ vue/vue_review.js
@@ -1,18 +1,38 @@
 const{createApp,ref}=Vue
 
-const vm=Vue.createApp({
-    template:`<div><p>{{ name }} = price :{{ price }} * quantity: {{ quantity }}= {{subtotal()}}</p></div>`,
+// const vm=Vue.createApp({
+//     template:`<div><p>{{ name }} = price :{{ price }} * quantity: {{ quantity }}= {{subtotal()}}</p></div>`,
+//     data(){
+//         return {
+//             name:"Jersey",price:"",quantity:""
+//         }
+//     },
+//     methods:{
+//         subtotal: function (){
+//             return this.price * this.quantity
+//         }
+//     }
+// }).mount("#temp")
+
+// vm.$data.price=79
+// vm.$data.quantity=23
+const am=Vue.createApp({
     data(){
-        return {
-            name:"Jersey",price:"",quantity:""
+        return{
+            twd:0.278
+            ,
+            customerid:"item-id"
         }
     },
-    methods:{
-        subtotal: function (){
-            return this.price * this.quantity
+    computed:{
+        jpy:{
+            get(){
+                return (this.twd/0.278).toFixed(3)
+            },
+            set(value){
+                this.twd=(value*0.278).toFixed(3)
+            }
         }
     }
-}).mount("#temp")
+}).mount("#app1")
 
-vm.$data.price=79
-vm.$data.quantity=23
