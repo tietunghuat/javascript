29f1beaa80df8f1b2d10ae4cc86975a34ca0f36a
Tie Tung Huat <tunghuat.2000@gmail.com>
2022-07-27 23:09:52 +0800

    7/27

--- vue/vue_review.js
+++ vue/vue_review.js
@@ -1,5 +1,13 @@
 const{createApp,ref}=Vue
+const vm = Vue.createApp({
+    data() {
+        return {
+            text: 'Hello',
+            text1: "<h1>NIcee world!</h1>"
 
+        }
+    }
+}).mount("#app1")
 // const vm=Vue.createApp({
 //     template:`<div><p>{{ name }} = price :{{ price }} * quantity: {{ quantity }}= {{subtotal()}}</p></div>`,
 //     data(){
@@ -16,51 +24,53 @@ const{createApp,ref}=Vue
 
 // vm.$data.price=79
 // vm.$data.quantity=23
-const am=Vue.createApp({
-    data(){
-        return{
-            twd:0.278
-            ,
-            customerid:"item-id"
-        }
-    },
-    computed:{
-        jpy:{
-            get(){
-                return (this.twd/0.278).toFixed(3)
-            },
-            set(value){
-                this.twd=(value*0.278).toFixed(3)
-            }
-        }
-    }
-}).mount("#app1")
+// const am=Vue.createApp({
+//     data(){
+//         return{
+//             twd:0.278
+//             ,
+//             customerid:"item-id"
+//         }
+//     },
+//     computed:{
+//         jpy:{
+//             get(){
+//                 return (this.twd/0.278).toFixed(3)
+//             },
+//             set(value){
+//                 this.twd=(value*0.278).toFixed(3)
+//             }
+//         }
+//     }
+// }).mount("#app1")
 
 
-const bm=Vue.createApp({
-    data()
-    {
-        return{
-            picked:1
-        }
-    }
-}).mount("#app2")
+// const bm=Vue.createApp({
+//     data()
+//     {
+//         return{
+//             picked:1
+//         }
+//     }
+// }).mount("#app2")
 
-const cm = Vue.createApp({
-    data() {
-        return {
-            picked: [1]
-        }
-    }
-}).mount("#app3")
+// const cm = Vue.createApp({
+//     data() {
+//         return {
+//             picked: [1]
+//         }
+//     }
+// }).mount("#app3")
 
-const dm = Vue.createApp({
-    data() {
-        return {
-            selected:""
-        }
-    }
-}).mount("#app4")
+// const dm = Vue.createApp({
+//     data() {
+//         return {
+//             selected:""
+//         }
+//     }
+// }).mount("#app4")
+
+//7/27
 
 
 
