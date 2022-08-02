632e9a9348885a547813762904d1f149562b67d3
Tie Tung Huat <tunghuat.2000@gmail.com>
2022-08-02 22:44:53 +0800

    8/2

--- vue/vue_eleventh.js
+++ vue/vue_eleventh.js
@@ -1,19 +1,35 @@
 const{createApp,ref}=Vue
 
-const am=Vue.createApp({
-    data(){
-        return{
-            isshow:true
-        }
-    }
-}).mount("#app1")
+// const am=Vue.createApp({
+//     data(){
+//         return{
+//             isshow:true
+//         }
+//     }
+// }).mount("#app1")
 
 
-const bm=Vue.createApp({
+// const bm=Vue.createApp({
+//     data(){
+//         return{
+//             count:0,
+//             st:"background-color:red;color:white;border:1px solid black;width:150px;height:100px auto;"
+//         }
+//     }
+// }).mount("#app2")
+
+const cm=Vue.createApp({
     data(){
         return{
-            count:0,
-            st:"background-color:red;color:white;border:1px solid black;width:150px;height:100px auto;"
+            tick:"picka"
         }
     }
-}).mount("#app2")
+}).mount("#app3")
+
+const dm=Vue.createApp({
+    data(){
+        return{
+             login:"username"
+        }
+    }
+}).mount("#app4")
---
+++ vue/vue_twelfth.js
@@ -0,0 +1,62 @@
+const{createApp,ref} = Vue  
+
+const vm=Vue.createApp({
+    data(){
+        return{
+            
+            arr:["nice","king","birdman","ice"]
+        }
+    }
+}).mount("#app1")
+
+const am=Vue.createApp({
+    data(){
+        return{
+            add:{title:"nice",name:"hi",date:"2019/9"}
+        }
+    }
+}).mount("#app2")
+
+const bm=Vue.createApp({
+    data(){
+        return{
+            numbers:[1,2,3,4,5,6,7,8]
+        }
+    },
+    computed:{
+        even(){
+            return this.numbers.filter(number => number %2 === 0)
+        }
+    }
+}).mount("#app3")
+
+const cm=Vue.createApp({
+    data(){
+        return{
+            list:[{
+                id:"task1",
+                title:"Choice 1",
+                isdone:false
+            },
+            {
+                id:"task2",
+                title:"Choice 2",
+                isdone:false
+            },
+            {
+                id:"task3",
+                title:"Choice 3",
+                isdone:false
+            }
+        ]}
+    },
+    computed:{
+        todolists(){
+            return this.list.filter(d => !d.isdone)
+        },
+        donelists() {
+            return this.list.filter(d => d.isdone)
+        }
+
+    }
+}).mount("#app4")
